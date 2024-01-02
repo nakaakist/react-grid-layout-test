040516bf559532a5261141f4c79ddc2421cd27e9
Nariyuki Saito <toshincompos@gmail.com>
2024-01-02 22:55:58 +0900

    Init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Responsive, WidthProvider } from "react-grid-layout";
+import "react-grid-layout/css/styles.css";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
+export const App = () => {
+  const layouts = {
+    sm: [
+      { i: "a", x: 0, y: 0, w: 1, h: 2 },
+      { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+      { i: "c", x: 4, y: 0, w: 2, h: 2 },
+    ],
+    xs: [
+      { i: "a", x: 0, y: 0, w: 1, h: 2 },
+      { i: "b", x: 1, y: 0, w: 1, h: 2 },
+      { i: "c", x: 2, y: 0, w: 1, h: 2 },
+    ],
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ResponsiveGridLayout
+      className="layout board"
+      breakpoints={{ sm: 698, xs: 0 }}
+      layouts={layouts}
+      cols={{ sm: 6, xs: 3 }}
+      rowHeight={150}
+    >
+      <div key="a" className="item">
+        a
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div key="b" className="item">
+        b
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+      <div key="c" className="item">
+        c
+      </div>
+    </ResponsiveGridLayout>
+  );
+};
