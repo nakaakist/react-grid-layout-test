5981fb1263b9e00273dd838250e03a6ba645a9b3
Nariyuki Saito <toshincompos@gmail.com>
2024-01-03 23:47:08 +0900

    Control layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,49 @@
-import { Responsive, WidthProvider } from "react-grid-layout";
+import { useState } from "react";
+import { Layouts, Responsive, WidthProvider } from "react-grid-layout";
 import "react-grid-layout/css/styles.css";
 import "./App.css";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
+const DEFAULT_LAYOUTS: Layouts = {
+  sm: [
+    { i: "a", x: 0, y: 0, w: 1, h: 2 },
+    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: "c", x: 4, y: 0, w: 2, h: 2 },
+  ],
+  xs: [
+    { i: "a", x: 0, y: 0, w: 1, h: 2 },
+    { i: "b", x: 1, y: 0, w: 1, h: 2 },
+    { i: "c", x: 2, y: 0, w: 1, h: 2 },
+  ],
+};
+
 export const App = () => {
-  const layouts = {
-    sm: [
-      { i: "a", x: 0, y: 0, w: 1, h: 2 },
-      { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-      { i: "c", x: 4, y: 0, w: 2, h: 2 },
-    ],
-    xs: [
-      { i: "a", x: 0, y: 0, w: 1, h: 2 },
-      { i: "b", x: 1, y: 0, w: 1, h: 2 },
-      { i: "c", x: 2, y: 0, w: 1, h: 2 },
-    ],
-  };
+  const [layouts, setLayouts] = useState<Layouts>(DEFAULT_LAYOUTS);
   return (
-    <ResponsiveGridLayout
-      className="layout board"
-      breakpoints={{ sm: 698, xs: 0 }}
-      layouts={layouts}
-      cols={{ sm: 6, xs: 3 }}
-      rowHeight={150}
-    >
-      <div key="a" className="item">
-        a
+    <div>
+      <ResponsiveGridLayout
+        className="layout board"
+        breakpoints={{ sm: 698, xs: 0 }}
+        layouts={layouts}
+        cols={{ sm: 6, xs: 3 }}
+        rowHeight={150}
+        onLayoutChange={(layout, layouts) => setLayouts(layouts)}
+      >
+        <div key="a" className="item">
+          a
+        </div>
+        <div key="b" className="item">
+          b
+        </div>
+        <div key="c" className="item">
+          c
+        </div>
+      </ResponsiveGridLayout>
+      <div className="data">
+        <pre>{JSON.stringify(DEFAULT_LAYOUTS, null, 2)}</pre>
+        <pre>{JSON.stringify(layouts, null, 2)}</pre>
       </div>
-      <div key="b" className="item">
-        b
-      </div>
-      <div key="c" className="item">
-        c
-      </div>
-    </ResponsiveGridLayout>
+    </div>
   );
 };
